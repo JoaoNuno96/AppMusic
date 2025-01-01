bd339266e25e334e1056317903395cd5dbd92b6b
JoaoNuno96 <joaonuno1996@hotmail.com>
2025-01-01 12:20:59 +0000

    Starting Log Entity

---
+++ AppMusic/Entities/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppMusic.Entities
+{
+    public class Log
+    {
+        public string Message { get; set; }
+        public string ErroMessage { get; set; }
+        public string ErrorCode { get; set; }
+        public bool Status { get; set; }
+        public override string ToString()
+        {
+            return Status ? SuccessMessage() : ErrorMessage();
+        }
+
+        public string SuccessMessage()
+        {
+            return Message;
+        }
+
+        public string ErrorMessage()
+        {
+            return ErrorCode + ErroMessage;
+        }
+    }
+}
--- AppMusic/obj/Debug/net8.0/AppMusic.AssemblyInfo.cs
+++ AppMusic/obj/Debug/net8.0/AppMusic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppMusic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd2dd36ac25e798d17d8af2c89bf92dd995f5ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d47fdb02af6c69058d1a51d9d65f5ba70742427")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppMusic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppMusic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
